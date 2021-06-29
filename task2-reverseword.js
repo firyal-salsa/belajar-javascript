f087e5cb2c38099a366286c8c2f139915d92c93f
firyal-salsa <53255114+firyal-salsa@users.noreply.github.com>
2021-06-30 01:46:07 +0700

    Update task2-reverseword.js

--- task2-reverseword.js
+++ task2-reverseword.js
@@ -1,18 +1,7 @@
-let reverseString = (str)=>{
-    // Step 1. Use the split() method to return a new array
-    var splitString = str.split(" ", 3); // var splitString = "hello".split("");
-    // ["h", "e", "l", "l", "o"]
+let reverseString = (kalimat)=>{
+    let r = kalimat.split(" ").reverse().join(' ')
  
-    // Step 2. Use the reverse() method to reverse the new created array
-    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
-    // ["o", "l", "l", "e", "h"]
- 
-    // Step 3. Use the join() method to join all elements of the array into a string
-    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
-    // "olleh"
-    
-    //Step 4. Return the reversed string
-    return joinArray; // "olleh"
+    return r
 }
  
-console.log(reverseString("saya belajar javascript"));
+console.log(reverseString("Saya belajar Javascript"));
