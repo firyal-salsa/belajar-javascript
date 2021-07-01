e11acb8aae46eeb92c5ab0ecd5e042ab44614bf2
firyal-salsa <53255114+firyal-salsa@users.noreply.github.com>
2021-07-01 18:48:26 +0700

    Update task5-divideandsort.js

--- task5-divideandsort.js
+++ task5-divideandsort.js
@@ -1,15 +1,3 @@
-/*
-                            Soal
-    1. Diberikan sebuah variabel yang berisikan bilangan integer dengan
-    2. ketentuan angka 0 (nol) dalam variabel tersebut merupakan pemisah antara satu bilangan dengan bilangan lainnya
-    3. Bilangan-bilangan tersebut akan dipisah dan diurutkan berdasarkan angka di bilangan-bilangan itu sendiri
-    4. Bilangan hasil pengurutan akan digabungkan kembali dengan tanpa pemisah dengan output berupa bilangan integer
-    5. Buatlah method/function yang menerima parameter hanya deret angka
-    6. Menghasilkan output angka saja
-
-*/
-
-
 /*Algoritma
 1. Mulai
 2. Buat variabel
@@ -20,8 +8,6 @@
 7. Output berupa bilangan integer
 8. Selesai
 */
-
-//map((num)=>{return Number(num)}).sort((a,b)=>a-b).join(' ')
 
 //Code
                                     
